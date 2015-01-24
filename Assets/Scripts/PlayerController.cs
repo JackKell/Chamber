9fdb53cfac9b2e4c75eb3d857998899f66575932
JackKell <Jackkell@Citadel>
2015-01-23 20:49:38 -0700

    Test Site

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,10 @@ using System.Collections;
 [RequireComponent(typeof(PlayerPhysics))]
 public class PlayerController : MonoBehaviour 
 {
+	public float gravity = 20;
 	public float speed = 8;
-	public float acceleration = 12;
+	public float acceleration = 30;
+	public float jumpHeight = 12;
 	
 	private float currentSpeed;
 	private float targetSpeed ;
@@ -22,24 +24,42 @@ public class PlayerController : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
+		if (playerPhysics.movementStopped)
+		{
+			targetSpeed = 0;
+			currentSpeed = 0;
+		}
+		
 		targetSpeed = Input.GetAxisRaw("Horizontal") * speed;
 		currentSpeed = IncrementTowards(currentSpeed, targetSpeed, acceleration);
 		
-		amountToMove = new Vector2(currentSpeed, 0);
+		if (playerPhysics.grounded)
+		{
+			amountToMove.y = 0;
+			
+			if (Input.GetButtonDown("Jump"))
+			{
+				amountToMove.y = jumpHeight;
+			}
+		}
+		
+		amountToMove.x = currentSpeed;
+		amountToMove.y -= gravity * Time.deltaTime;
 		playerPhysics.Move(amountToMove * Time.deltaTime);
 	}
 	
-	private float IncrementTowards(float n, float target, float a)
+	private float IncrementTowards(float currentSpeed, float targetSpeed, float acceleration)
 	{
-		if (n == target)
+		if (currentSpeed == targetSpeed)
 		{
-			return n;
+			return currentSpeed;
 		}
 		else
 		{
-			float dir = Mathf.Sign (target - n);
-			n += a * Time.deltaTime * dir;
-			return (dir == Mathf.Sign(target-n))? n: target;
+			//Math.Sign states whether a number is negative or positive
+			float direction = Mathf.Sign (targetSpeed - currentSpeed);
+			currentSpeed += acceleration * Time.deltaTime * direction;
+			return (direction == Mathf.Sign(targetSpeed-currentSpeed))? currentSpeed: targetSpeed;
 		}
 	}
 }
--- Assets/Scripts/PlayerPhysics.cs
+++ Assets/Scripts/PlayerPhysics.cs
@@ -1,10 +1,95 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(BoxCollider))]
 public class PlayerPhysics : MonoBehaviour {
-
+	
+	public LayerMask collisionMask;
+	public bool grounded;
+	public bool movementStopped;
+	
+	private BoxCollider collider;
+	private Vector3 size;
+	private Vector3 center;
+	
+	private float skin = .00001f;
+	
+	private Ray ray;
+	private RaycastHit hit;
+	
+	public void Start()
+	{
+		collider = GetComponent<BoxCollider>();
+		size = collider.size;
+		center = collider.center;
+	}
+	
 	public void Move(Vector2 moveAmount)
 	{
-		transform.Translate(moveAmount);
+		float deltaY = moveAmount.y;
+		float deltaX = moveAmount.x;
+		Vector2 position = transform.position;
+		
+		//Checks Up and Down Collision
+		grounded = false;
+		for (int i = 0; i < 3; i++)
+		{
+			float direction = Mathf.Sign(deltaY);
+			float x = (position.x + center.x - size.x/2) + size.x/2 * i;
+			float y = position.y + center.y + size.x/2 * direction;
+						
+			ray = new Ray(new Vector2(x, y), new Vector2(0, direction));
+			Debug.DrawRay(ray.origin, ray.direction);
+			
+			if (Physics.Raycast(ray,out hit,Mathf.Abs(deltaY) + skin,collisionMask))
+			{
+				float distance = Vector3.Distance(ray.origin, hit.point);
+				
+				if (distance > skin)
+				{
+					deltaY = distance * direction - skin * direction;				
+				}
+				else
+				{		
+					deltaY = 0;
+				}
+				grounded = true;
+				break;
+			}
+		}
+		
+		//Checks Right and Left Collision
+		movementStopped = false;
+		for (int i = 0; i < 3; i++)
+		{
+			float direction = Mathf.Sign(deltaX);
+			Debug.Log("LR Direction: " + direction);
+			float y = (position.y + center.y - size.y/2) + size.y/2 * i;
+			float x = position.x + center.x + size.y/2 * direction;
+			
+			ray = new Ray(new Vector2(x, y), new Vector2(direction, 0));
+			Debug.DrawRay(ray.origin, ray.direction);
+			
+			if (Physics.Raycast(ray,out hit,Mathf.Abs(deltaX) + skin,collisionMask))
+			{
+				Debug.Log("Fire");
+				float distance = Vector3.Distance(ray.origin, hit.point);
+				
+				if (distance > skin)
+				{
+					deltaX = distance * direction - skin * direction;				
+				}
+				else
+				{		
+					deltaX = 0;
+				}
+				movementStopped = true;
+				break;
+			}
+		}
+		
+		Vector2 finalTransform = new Vector2(deltaX, deltaY);
+		
+		transform.Translate(finalTransform);
 	}
 }
