b665ee6a84dd550c263c47aa6e69a804e02d886c
JackKell <Jackkell@Citadel>
2015-01-24 23:00:59 -0700

    Title, Credits, Manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour 
+{
+	static GameManager _instance;
+	
+	static public bool isActive 
+	{
+		get 
+		{
+			return _instance != null;
+		}
+	}
+	
+	static public GameManager instance
+	{
+		get
+		{
+			if (_instance == null)
+			{
+				_instance = Object.FindObjectOfType(typeof(GameManager)) as GameManager;
+				
+				if (_instance == null)
+				{
+					GameObject go = new GameObject("_gamemanager");
+					DontDestroyOnLoad(go);
+					_instance = go.AddComponent<GameManager>();
+				}
+			}
+			return _instance;
+		}
+	}
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
+	
+	}
+	
+	public void LoadTitleScene()
+	{
 	
 	}
 }
