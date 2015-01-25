d84f2300832dce80b4ce60deceb78730e8be1297
FunnyGopher <FunnyGopher@Hermes>
2015-01-24 21:13:36 -0700

    We now have saws!

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Grid : MonoBehaviour {
+
+	public List<GameObject> panels = new List<GameObject>();
+	private int width = 18;
+	private int height = 10;
+	// Use this for initialization
+	void Start () {
+		startConfig ();
+		toggleCells (false, new Vector2 (9, 1), new Vector2 (9, 2));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void startConfig() {
+		for(int i = 0; i < width * height; i++) {
+			PanelToggle pt = panels[i].GetComponent<PanelToggle>();
+			pt.deactivate(false);
+		}
+
+		toggleRow (0, true, false);
+		toggleRow (height - 1, true, false);
+
+		toggleColumn (0, true, false);
+		toggleColumn (width - 1, true, false);
+	}
+
+	public void toggleColumn(int colNumber, bool state, bool lerp = true) {
+		for(int i = 0; i < height; i++) {
+			PanelToggle pt = panels[colNumber + i * width].GetComponent<PanelToggle>();
+			
+			if(state)
+				pt.activate(lerp);
+			else
+				pt.deactivate(lerp);
+		}
+	}
+
+	public void toggleRow(int rowNumber, bool state, bool lerp = true) {
+		for(int i = 0; i < width; i++) {
+			PanelToggle pt = panels[i + rowNumber * width].GetComponent<PanelToggle>();
+
+			if(state)
+				pt.activate(lerp);
+			else
+				pt.deactivate(lerp);
+		}
+	}
+
+	public void toggleCell(int rowNumber, int colNumber, bool state, bool lerp = true) {
+		PanelToggle pt = panels[colNumber + rowNumber * width].GetComponent<PanelToggle>();
+		
+		if(state)
+			pt.activate(lerp);
+		else
+			pt.deactivate(lerp);
+	}
+
+	public void toggleCells(bool state, params Vector2[] cells) {
+		foreach(Vector2 cell in cells) {
+			toggleCell ((int)cell.x, (int)cell.y, state);
+		}
+	}
+}
---
+++ Assets/Scripts/PanelToggle.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanelToggle : MonoBehaviour {
+
+	public bool isActive = true;
+	public float alpha = .5f;
+	public float delta = .01f;
+
+	private BoxCollider collider;
+	private SpriteRenderer renderer;
+	private float targetAlpha = 1.0f;
+
+	private float r;
+	private float g;
+	private float b;
+	private float a;
+
+	// Use this for initialization
+	void Start () {
+		collider = GetComponent<BoxCollider> ();
+		renderer = GetComponent<SpriteRenderer> ();
+		r = renderer.color.r;
+		g = renderer.color.g;
+		b = renderer.color.b;
+		a = renderer.color.a;
+
+		if(isActive == false) {
+			deactivate();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		r = renderer.color.r;
+		g = renderer.color.g;
+		b = renderer.color.b;
+		a = renderer.color.a;
+		lerpAlpha (targetAlpha);
+
+		collider.enabled = isActive;
+	}
+
+	public void lerpAlpha(float targetAlpha) {
+		a = Mathf.Lerp (a, targetAlpha, delta);
+		renderer.color = new Color (r, g, b, a);
+	}
+	
+	public void toggle() {
+		if (isActive) {
+			deactivate ();
+		}
+		else {
+			activate();
+		}
+	}
+	
+	public void activate(bool lerp = true) {
+		isActive = true;
+		targetAlpha = 1.0f;
+		renderer.sortingOrder = 1;
+
+		if (lerp == false) {
+			renderer.color = new Color (r, g, b, targetAlpha);
+		}
+	}
+	
+	public void deactivate(bool lerp = true) {
+		isActive = false;
+		targetAlpha = this.alpha;
+		renderer.sortingOrder = 0;
+
+		if(lerp == false) {
+			renderer.color = new Color (r, g, b, targetAlpha);
+		}
+	}
+}
