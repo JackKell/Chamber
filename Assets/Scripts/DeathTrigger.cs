725b59c561006418375caa8d7dc1ce24b1224e5f
FunnyGopher <FunnyGopher@Hermes>
2015-01-24 17:02:43 -0700

    Created a death trigger object

---
+++ Assets/Scripts/DeathTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathTrigger : MonoBehaviour {
+
+	public string tagName = "";
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		if(collider.tag == tagName) {
+			Destroy (this.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/EntityComponent.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EntityComponent : MonoBehaviour {
+
+	public bool isAlive;
+	public int health = 0;
+
+	// Use this for initialization
+	void Start () {
+		isAlive = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (health <= 0) {
+			kill();
+		}
+
+		if(isAlive == false) {
+			destroy();
+		}
+	}
+
+	public void damage(int damage) {
+		health -= damage;
+	}
+
+	public void kill() {
+		isAlive = false;
+	}
+
+	public void destroy() {
+		Destroy(this.gameObject);
+	}
+}
