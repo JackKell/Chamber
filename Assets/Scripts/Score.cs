86ffd1488c5089cb3fb62d6802fc3f5bef1f4e12
FunnyGopher <FunnyGopher@Hermes>
2015-01-24 22:16:56 -0700

    Created WIP score script

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+	public float score = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		score += Time.deltaTime;
+	}
+}
